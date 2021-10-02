a874aba4a7e34847056745cdacff457555908a42
patrick <patricksanzo@gmail.com>
2021-10-02 21:54:15 +0200

    docker: Correct image and tag and extract

--- ubuntu-latex/compile-document.sh
+++ ubuntu-latex/compile-document.sh
@@ -2,9 +2,10 @@
 
 FOLDER_WITH_TEX_FILES=$1
 TEX_FILE_PDF_SUFFIX=$2
-
+IMAGE_TAG=0.1
+IMAGE=patsancu/latex-ubuntu
 
 docker run -it --rm \
     -e TEX_FILE_PDF_SUFFIX=$TEX_FILE_PDF_SUFFIX \
-    -v $FOLDER_WITH_TEX_FILES:/tex_files ubuntu-latex \
+    -v $FOLDER_WITH_TEX_FILES:/tex_files $IMAGE:$IMAGE_TAG \
     sh -c "cd /tex_files && rm -f $TEX_FILE_PDF_SUFFIX &&  make clean && make $TEX_FILE_PDF_SUFFIX"
